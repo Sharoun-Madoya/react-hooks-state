bfc38134cd7ae6d60d62470326052249be7426eb
[sharoun <[madoyasharoun@gmail.com]>
2023-04-21 16:41:48 +0300

    Completed assignment

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,10 +4,15 @@ function Counter() {
   const [count, setCount] = useState(0);
 
   function increment() {
-    setCount(count + 1);
-  }
+    // // console.log(`before setState: ${count}`);
+    setCount((currentCount) => currentCount + 1);
+    setCount((currentCount) => currentCount + 1);
+    // setCount(count + 1);
 
-  return <button onClick={increment}>I have been clicked {count} times</button>;
+    // // console.log(`after setstate: ${count}`);
+  }
+  // return <div onClick={increment}>{count}</div>;
+
 }
 
 export default Counter;
